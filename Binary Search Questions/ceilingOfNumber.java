4a268c5c251fe470265fa25cf6a909f49dc922fe
Goldee Ji <76255743+GoldeeArora@users.noreply.github.com>
2021-12-25 14:38:25 +0530

    Add files via upload

---
+++ Binary Search Questions/ceilingOfNumber.java
@@ -0,0 +1,7 @@
+// smallest element in the array that is equal to or greater than the target number
+class ceilingOfNumber{
+
+}
+https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+// find smallest letter greater than target 
+// it is based on the binary search concept
---
+++ Binary Search Questions/notes.java
@@ -0,0 +1,1 @@
+// Whenever you see a sorted array Try to solve the problem with the binary search First
